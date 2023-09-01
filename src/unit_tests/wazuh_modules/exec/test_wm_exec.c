0dc26056077262a72e72bf54fefcef99ebfcd019
nbertoldo <nicobertoldo@gmail.com>
2023-09-01 18:29:06 -0300

    Unit tests added for wm_exec.c

--- src/unit_tests/wazuh_modules/exec/test_wm_exec.c
+++ src/unit_tests/wazuh_modules/exec/test_wm_exec.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015, Wazuh Inc.
+ * Copyright (C) 2023, Wazuh Inc.
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General Public
@@ -19,16 +19,26 @@
 
 #include "../../wrappers/wazuh/shared/list_op_wrappers.h"
 
-static void setup_modules(void ** state) {
+static int setup_modules(void ** state) {
     *state = NULL;
     wm_children_pool_init();
+    return 0;
 }
 
-static void teardown_modules(void ** state) {
+static int teardown_modules(void ** state) {
     wm_children_pool_destroy();
+    return 0;
 }
 
 #ifndef TEST_WINAGENT
+static void test_wm_append_sid_null_list(void ** state) {
+    pid_t sid = 10;
+
+    expect_string(__wrap__merror, formatted_msg, "Child process sid 10 could not be registered.");
+
+    wm_append_sid(sid);
+}
+
 static void test_wm_append_sid_fail(void ** state) {
 
     pid_t sid = 10;
@@ -57,7 +67,7 @@ static void test_wm_remove_sid_null_list(void ** state) {
 
     expect_string(__wrap__merror, formatted_msg, "Child process 10 not found.");
 
-    wm_append_sid(sid);
+    wm_remove_sid(sid);
 }
 
 static void test_wm_remove_sid_not_found(void ** state) {
@@ -66,7 +76,25 @@ static void test_wm_remove_sid_not_found(void ** state) {
     will_return(__wrap_OSList_GetFirstNode, NULL);
     expect_string(__wrap__merror, formatted_msg, "Child process 10 not found.");
 
-    wm_append_sid(sid);
+    wm_remove_sid(sid);
+}
+
+static void test_wm_remove_sid_success(void ** state) {
+    pid_t sid = 10;
+    pid_t * p_sid = NULL;
+    OSListNode *node;
+
+    os_calloc(1, sizeof(pid_t), p_sid);
+    *p_sid = sid;
+    node = (OSListNode *) calloc(1, sizeof(OSListNode));
+    node->data = p_sid;
+
+    will_return(__wrap_OSList_GetFirstNode, node);
+    expect_function_call(__wrap_OSList_DeleteThisNode);
+
+    wm_remove_sid(sid);
+    
+    os_free(node);
 }
 
 #else
@@ -86,10 +114,12 @@ static void test_wm_kill_children_win(void ** state) {
  int main() {
     const struct CMUnitTest tests[] = {
 #ifndef TEST_WINAGENT
+        cmocka_unit_test_setup_teardown(test_wm_append_sid_null_list, NULL, NULL),
         cmocka_unit_test_setup_teardown(test_wm_append_sid_fail, setup_modules, teardown_modules),
         cmocka_unit_test_setup_teardown(test_wm_append_sid_success, setup_modules, teardown_modules),
         cmocka_unit_test_setup_teardown(test_wm_remove_sid_null_list, NULL, NULL),
         cmocka_unit_test_setup_teardown(test_wm_remove_sid_not_found, setup_modules, teardown_modules),
+        cmocka_unit_test_setup_teardown(test_wm_remove_sid_success, setup_modules, teardown_modules)
 #else
         cmocka_unit_test_setup_teardown(test_wm_append_handle, setup_modules, teardown_modules),
         cmocka_unit_test_setup_teardown(test_wm_remove_handle, setup_modules, teardown_modules),
