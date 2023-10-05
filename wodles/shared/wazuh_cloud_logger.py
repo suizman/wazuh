5981c8fc79acc3794117b71db2447f04fc0d6a9c
Eduardo <eduardo.leon@wazuh.com>
2023-10-05 16:00:02 -0300

    Add cloud logger

--- wodles/shared/wazuh_cloud_logger.py
+++ wodles/shared/wazuh_cloud_logger.py
@@ -1,104 +1,50 @@
-from abc import ABC, abstractmethod
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# Copyright (C) 2015, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute
+# it and/or modify it under the terms of GPLv2
+
+"""This module implements a logging solution for Wazuh Cloud Modules."""
+
 import logging
 import sys
 
 
-class LogStrategy(ABC):
+class WazuhCloudLogger:
     """
-    LogStrategy interface.
-    Defines the methods that need to be implemented by the concrete classes
-    (GCP, AWS, and Azure) for logging.
-    """
-
-    @abstractmethod
-    def info(self, message: str):
-        """
-        Log an INFO level message.
-        Parameters
-        ----------
-        message : str
-            The message to be logged.
-        """
-        pass
-
-    @abstractmethod
-    def debug(self, message: str):
-        """
-        Log a DEBUG level message.
-        Parameters
-        ----------
-        message : str
-            The message to be logged.
-        """
-        pass
-
-    @abstractmethod
-    def warning(self, message: str):
-        """
-        Log a WARNING level message.
-        Parameters
-        ----------
-        message : str
-            The message to be logged.
-        """
-        pass
-
-    @abstractmethod
-    def error(self, message: str):
-        """
-        Log an ERROR level message.
-        Parameters
-        ----------
-        message : str
-            The message to be logged.
-        """
-        pass
-
-
-class Logger:
-    """
-    Logger class.
-    Provides a flexible logging solution for Wazuh that supports
-    GCP, AWS, and Azure integrations using the strategy pattern.
+    WazuhCloudLogger class.
+    Provides a standardized logging solution for Wazuh.
     """
 
-    def __init__(self, strategy: LogStrategy, logger_name: str, log_level=1):
+    def __init__(self, logger_name: str):
         """
-        Initialize the Logger class.
+        Initialize the Wazuh Cloud Logger class.
         Parameters
         ----------
-        strategy : LogStrategy
-            The logging strategy to be used (GCP, AWS, or Azure).
         logger_name : str
-            The name of the logger.
-        log_level : int, optional
-            The logging level (0 for WARNING, 1 for INFO, 2 for DEBUG), default is 1.
+            The logging name to be used.
         """
-        self.strategy = strategy
-        self.logger = self.setup_logger(logger_name, log_level)
+        self.logger_name = logger_name
+        self.logger = self.setup_logger()
 
-    def setup_logger(self, logger_name: str, log_level: int) -> logging.Logger:
+    def setup_logger(self) -> logging.Logger:
         """
         Set up the logger.
-        Parameters
-        ----------
-        logger_name : str
-            The name of the logger.
-        log_level : int
-            The logging level (0 for WARNING, 1 for INFO, 2 for DEBUG).
         Returns
         -------
         logging.Logger
             Configured logger instance.
         """
-        logger = logging.getLogger(logger_name)
-        log_levels = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
-        logger.setLevel(log_levels.get(log_level, logging.INFO))
+        logger = logging.getLogger(self.logger_name)
+        logger.setLevel(logging.INFO)
         handler = self._setup_handler()
         logger.addHandler(handler)
         return logger
 
-    def _setup_handler(self) -> logging.Handler:
+    @staticmethod
+    def _setup_handler() -> logging.Handler:
         """
         Set up the handler for the logger.
         Returns
@@ -118,7 +64,7 @@ class Logger:
         message : str
             The message to be logged.
         """
-        self.strategy.info(message)
+        self.logger.info(message)
 
     def debug(self, message: str):
         """
@@ -128,7 +74,7 @@ class Logger:
         message : str
             The message to be logged.
         """
-        self.strategy.debug(message)
+        self.logger.debug(message)
 
     def warning(self, message: str):
         """
@@ -138,14 +84,28 @@ class Logger:
         message : str
             The message to be logged.
         """
-        self.strategy.warning(message)
+        self.logger.warning(message)
 
-    def error(self, message: str):
+    def error(self, message: str, log_exception: bool = False):
         """
         Log an ERROR level message using the selected strategy.
         Parameters
         ----------
         message : str
             The message to be logged.
+        log_exception : bool
+            A boolean to log more information about the exception
         """
-        self.strategy.error(message)
+        self.logger.error(message,
+                          exc_info=log_exception)
+
+    def set_level(self, log_level: int):
+        """
+        Set the logging level for the logger used by the strategy.
+        Parameters
+        ----------
+        log_level : int
+            The logging level to be set.
+        """
+        logger_level = logging.DEBUG if log_level == 2 else logging.INFO
+        self.logger.setLevel(logger_level)
